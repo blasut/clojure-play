44fb79e31563819995e27d451c98cd2b394c5f69
blasut <blasut937@gmail.com>
2017-10-23 20:40:33 +0200

    add test.check

--- pcl/project.clj
+++ pcl/project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta2"]]
+  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
+                 [org.clojure/test.check "0.10.0-alpha2"]]
   :main ^:skip-aot pcl.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
