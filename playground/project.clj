04559cfd63fa84ef634d406015d7b6eec6381c6e
blasut <blasut937@gmail.com>
2017-11-04 23:53:06 +0100

    add stuff

--- playground/project.clj
+++ playground/project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0-beta4"]
+                 [org.clojure/spec.alpha "0.1.143"]
+                 [org.clojure/test.check "0.10.0-alpha2"]
+                 [com.cognitect/transcriptor "0.1.5"]])
--- playground/src/playground/core.clj
+++ playground/src/playground/core.clj
@@ -1,6 +1,24 @@
-(ns playground.core)
+(ns playground.core
+  (:require [cognitect.transcriptor :as xr]))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(xr/repl-files ".")
+
+(+ 5 5)
+
+(xr/run (first (xr/repl-files ".")))
+
+
+(def a 5)
+(def b 10)
+(def c 15)
+
+
+
+
+
+
