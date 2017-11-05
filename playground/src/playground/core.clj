07b86048572566f376b52d425bb3d769378c0b02
blasut <blasut937@gmail.com>
2017-11-05 02:01:48 +0100

    built in inspector hehe

--- playground/src/playground/core.clj
+++ playground/src/playground/core.clj
@@ -1,5 +1,6 @@
 (ns playground.core
-  (:require [cognitect.transcriptor :as xr]))
+  (:require [cognitect.transcriptor :as xr]
+            [clojure.inspector :as ins]))
 
 (defn foo
   "I don't do a whole lot."
@@ -12,10 +13,23 @@
 
 (xr/run (first (xr/repl-files ".")))
 
-
 (def a 5)
 (def b 10)
 (def c 15)
+
+(ins/inspect-tree {:a 1 :b 2 :c [1 2 3 {:d 4 :e 5 :f [8 7 5]}]})
+
+(ins/inspect-table [[1 2 3] [4 5 6] [7 8 9 2 4 5]])
+
+(ins/inspect (System/getProperties))
+
+
+
+
+
+
+
+
 
 
 
