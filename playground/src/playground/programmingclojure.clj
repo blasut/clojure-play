8f62a43f2e95af54750e48e3eab99bb3ab87cdb9
blasut <blasut937@gmail.com>
2017-11-11 15:27:29 +0100

    fucking maze

--- playground/src/playground/programmingclojure.clj
+++ playground/src/playground/programmingclojure.clj
@@ -364,6 +364,12 @@
      (populate (empty-board 6 6))
      pprint/pprint)
 
+(->> (iterate (stepper neighbours #{3} #{2 3}) #{[2 0] [2 1] [2 2] [1 2] [0 1]})
+     (drop 8)
+     first
+     (populate (empty-board 6 6))
+     pprint/pprint)
+
 
 ;; neighbours is the only part that cares about the content of the cells, and is depended on their structure
 
@@ -395,6 +401,70 @@
 
 (hex-step #{[0 0] [1 1] [1 3] [0 4]})
 ;; this returns the correct result, but in reversed order...
+
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Maze generation
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; using a vector of [x y] to represent a coordinate is called a "natural identifier"
+;; natural identifier = ???
+;; natural identifier is maybe a identifier that is itself the thing?
+
+;; location = [x y]
+;; visited locations = #{locations}
+;; a wall = #{[[0 0] [1 0]]} = a wall between those coordinates, we use a set because [[0 0] [1 0]] == [[1 0] [0 0]]
+;; a maze = #{walls}
+;; a walk through the maze is a seq of locations
+;; exit direction = [from to] order matters, have to be vectors
+
+(defn maze
+  "Returns a random maze carved out of walls; walls is a set of 2-item sets #{a b} where a and b are locations.
+  The returned maze is a set of the remaining walls."
+  [walls]
+  (let [paths (reduce (fn [index [a b]]
+                        (merge-with into index {a [b] b [a]}))
+                      {} (map seq walls))
+        start-loc (rand-nth (keys paths))]
+    (loop [walls walls
+           unvisited (disj (set (keys paths)) start-loc)]
+      (if-let [loc (when-let [s (seq unvisited)] (rand-nth s))]
+        (let [walk (iterate (comp rand-nth paths) loc)
+              steps (zipmap (take-while unvisited walk) (next walk))]
+          (recur (reduce disj walls (map set steps))
+                 (reduce disj unvisited (keys steps))))
+        walls))))
+
+(defn grid
+  [w h]
+  (set (concat
+        (for [i (range (dec w)) j (range h)] #{[i j] [(inc i) j]})
+        (for [i (range w) j (range (dec h))] #{[i j] [i (inc j)]}))))
+
+(defn draw
+  [w h maze]
+  (doto (javax.swing.JFrame. "Maze")
+    (.setContentPane
+     (doto (proxy [javax.swing.JPanel] []
+             (paintComponent [^java.awt.Graphics g]
+               (let [g (doto ^java.awt.Graphics2D (.create g)
+                         (.scale 10 10)
+                         (.translate 1.5 1.5)
+                         (.setStroke (java.awt.BasicStroke. 0.4)))]
+                 (.drawRect g -1 -1 w h)
+                 (doseq [[[xa ya] [xb yb]] (map sort maze)]
+                   (let [[xc yc] (if (= xa xb)
+                                   [(dec xa) ya]
+                                   [xa (dec ya)])]
+                     (.drawLine g xa ya xc yc))))))
+       (.setPreferredSize (java.awt.Dimension.
+                           (* 10 (inc w)) (* 10 (inc h))))))
+    .pack
+    (.setVisible true)))
+
+(draw 40 40 (maze (grid 40 40)))
+
+
 
 
 
