d53e41818ee30b520953f3effd93e22666e88e2c
blasut <blasut937@gmail.com>
2017-11-19 11:44:01 +0100

    add transducer version

--- playground/src/playground/etl_pipelines.clj
+++ playground/src/playground/etl_pipelines.clj
@@ -63,6 +63,62 @@
 
   (time (process (repeat 8 file-name))))
 
+(comment
+  (def num-coll (range 10))
+
+  ;; the two expressions are equal, but the transducer one doesn't create any intermediate sequence between filtering and mapping
+  (map inc (filter even? num-coll))
+  ;; sequence returns a lazy seq... should be possible to take?
+  (sequence (comp (filter even?) (map inc)) num-coll)
+  (take 1 (sequence (comp (filter even?) (map inc)) num-coll))
+  (type (sequence (comp (filter even?) (map inc)) num-coll))
+  ;; => lazy-seq
+  )
+
+;; someting like line-seq but returns something reducible instead of a lazy seq
+;; the ^-thing is a reader macro(?) that means meta-data. In this case it means a typehint
+(defn lines-reducible [^java.io.BufferedReader rdr]
+  (reify clojure.lang.IReduceInit
+    (reduce [this f init]
+      (try
+        (loop [state init]
+          (if (reduced? state)
+            state
+            (if-let [line (.readLine rdr)]
+              (recur (f state line))
+              state)))
+        (finally (.close rdr))))))
+
+(meta #'lines-reducible)
+;; doens't return the type hint though. Must be in the inner form?
+
+;; eduction = a way to call transducers on stuff, several transform-fns can be given without comp
+;; it's not realizing the coll, but returns an Eduction object which knows how to perform the transforms.
+;; when printing it prints as a seq, but it's really a Eduction obj.
+;; ???
+(defn parse-json-file-reducible [file]
+  (eduction (map #(json/decode % true))
+            (lines-reducible (io/reader file))))
+
+
+(defn process-with-transducers [files]
+  (transduce (comp (mapcat parse-json-file-reducible)
+                   (filter valid-entry?)
+                   (keep transform-entry-if-relevant)
+                   (partition-all 1000)
+                   (map save-into-database))
+             (constantly nil)           ;; the accumulator fn, but we don't want to return anything
+             nil                        ;; the starting value is also nil
+             files))
+
+(comment
+  (time (process-with-transducers [file-name]))
+  (time (process-with-transducers (repeat 8 file-name))))
+
+;; we can use core async with transducers to parallelize thi shiet
+
+
+
 
 
 
