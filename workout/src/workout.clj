022b83dbf6d4b5ad6b073d4ad3886ed02736fa4a
blasut <blasut937@gmail.com>
2018-06-13 22:24:10 +0200

    trying out speccing workout

---
+++ workout/src/workout.clj
@@ -0,0 +1,27 @@
+(ns workout
+  (:require [clojure.spec.alpha :as s]))
+
+(s/valid? number? 44)
+
+(s/def ::sets (s/and int? #(> % 0)))
+(s/def ::reps (s/and int? #(> % 0)))
+(s/def ::number (s/and int? #(> % 0)))
+;; (s/def ::unit (s/or :kg :lg))
+(s/def ::unit keyword?)
+(s/def ::weight (s/keys :req [::unit ::number]))
+(s/def ::name string?)
+
+(let [exercise (s/keys :req-un [::sets
+                                ::reps
+                                ::name])
+      pass (s/coll-of exercise)
+      schema (s/coll-of pass)
+      completed-exercise (s/keys :req-un [::sets ::reps ::weight])
+      completed-pass (s/and pass (s/coll-of completed-exercise))]
+  (s/explain exercise {:sets 5 :reps 5 :name "Squat"})
+  (println (s/exercise completed-pass))
+  (s/exercise completed-exercise))
+
+
+
+
