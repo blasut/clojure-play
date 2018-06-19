49b08dec51e288c1bd7a27fd13f6dae87ee75c39
blasut <blasut937@gmail.com>
2018-06-19 23:13:35 +0200

    parsing the strings
    jesus this is ugly

--- workout/src/workout.clj
+++ workout/src/workout.clj
@@ -1,5 +1,7 @@
 (ns workout
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.spec.alpha :as s]
+            [clojure.string :as str]
+            [clojure.pprint :as pprint]))
 
 (s/valid? number? 44)
 
@@ -21,6 +23,37 @@
   (s/explain exercise {:sets 5 :reps 5 :name "Squat"})
   (println (s/exercise completed-pass))
   (s/exercise completed-exercise))
+
+
+(let [pass {:date "2018-19-6" :name "A" :week 1
+            :exercises [{:name "Squat"
+                         :expected [5 5 50]
+                         :set-reps-weight [[1 5 20]
+                                           [2 5 40]
+                                           [3 5 30]
+                                           [4 5 30]
+                                           [5 5 30]]}]}
+      input-pass {:date "2018-19-6" :name "A" :week 1
+                  :exercises [{:name "Squat"
+                               :expected "50*5*5"
+                               :set-reps-weight "1/5-20\n2/5-40\n3/5-30\n4/5-30\n5/5-30\n"}]}
+      parse-pass (fn [raw-pass]
+                   (let [s-r-w (map (fn [e]
+                                      (map #(str/split % #"/|-")
+                                           (:set-reps-weight (update e :set-reps-weight #(str/split % #"\n")))))
+                                    (:exercises raw-pass))]
+                     s-r-w))]
+  (pprint/pprint (:exercises input-pass))
+  (pprint/pprint (parse-pass input-pass)))
+
+(map #(str "Hello " % "!") ["Ford" "Arthur" "Tricia"])
+
+
+
+
+
+
+
 
 
 
