21dc8c9a6d85d3ecb0fa45b853570018304b848b
blasut <blasut937@gmail.com>
2018-06-24 12:31:46 +0200

    add notes

--- workout/src/workout.clj
+++ workout/src/workout.clj
@@ -55,6 +55,48 @@
 
 (map #(str "Hello " % "!") ["Ford" "Arthur" "Tricia"])
 
+(comment
+  ;; I would like to have the following interface:
+  ;; Schema   = [Pass] [Rules]
+  ;; Pass     = [Exercises] Name [Rules] ;; Rules specific to that Pass, can override the Schema???
+  ;; Exercise = Name Set/Rep [Rules] || Exercise = Name Description [Rules] ;; For example if you have an HiT exercise on the bike
+  ;; Set/Rep = (Sets, Reps)
+  ;; Rules = ??? ;; Maybe keys that point to hashmap-dispatcher?
+  ;; CompletedPass/Exercise-Occasion/Workout = Pass [CompletedExercises] Date BodyWeight
+  ;; CompletedExercise = Exercise [Set/Rep/Weight] Comment Video
+  (add-schema schema)
+
+  (add-workout completed-pass)
+
+  (generate-workout-from schema)
+
+  (generate-next-workout-from schema [workouts])
+
+  (workouts->progression-graph [workouts])
+
+  ;; Rules
+  ;; Examples of rules:
+  ;; Increase weight by 2.5kg per workout, based on the previous workout
+  ;; Increase weight by 5kg per workout, until 100kg then 2.5kg, based on the previous workout
+  ;; Alternative pass each week, week1: A,B,A, week2: B,A,B
+  ;; Sprint for 10 seconds, reaching 90-95% of max pulse
+  ;; Do workout X times per week
+
+  ;; Warmup
+  ;; Can warmup be described as a "rule"?
+  ;; The warmup should be generated when generating a workout
+  ;; Warmup:
+  ;; - Start with lowest weight for that exercise,
+  ;; - Do 2 sets of 5 reps each
+  ;; - Then add between 5 to 10kg
+  ;; - Do 2 sets of 5 reps each
+  ;; - Repeat until next last warmup weight
+  ;; - Then do 1 set of 5 reps
+  ;; - Then add 5 to 10 kg
+  ;; - Then do 1 set of 1 rep
+  ;; Generate-Warmup ???
+  )
+
 
 
 
