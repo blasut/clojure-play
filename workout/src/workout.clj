1df8aed0ee8678fcf75c794ba94671ffdb70e49b
blasut <blasut937@gmail.com>
2018-06-27 13:31:38 +0200

    move specs into spec namespace

--- workout/src/workout.clj
+++ workout/src/workout.clj
@@ -3,27 +3,6 @@
             [clojure.string :as str]
             [clojure.pprint :as pprint]))
 
-(s/valid? number? 44)
-
-(s/def ::sets (s/and int? #(> % 0)))
-(s/def ::reps (s/and int? #(> % 0)))
-(s/def ::number (s/and int? #(> % 0)))
-(s/def ::unit #{:kg :lb})
-(s/def ::unit keyword?)
-(s/def ::weight (s/keys :req [::unit ::number]))
-(s/def ::name string?)
-
-(let [exercise (s/keys :req-un [::sets
-                                ::reps
-                                ::name])
-      pass (s/coll-of exercise)
-      schema (s/coll-of pass)
-      completed-exercise (s/keys :req-un [::sets ::reps ::weight])
-      completed-pass (s/and pass (s/coll-of completed-exercise))]
-  (s/explain exercise {:sets 5 :reps 5 :name "Squat"})
-  (println (s/exercise completed-pass))
-  (s/exercise completed-exercise))
-
 
 (defn- parse-s-r-w [e]
   (let [split-into-s-r-w #(str/split % #"/|-")]
@@ -164,6 +143,7 @@
                                        "Overhead Press" 20
                                        "Deadlift" 50}
                                0))
+
   (gen-warmup (get-in schema [:pass 0 :exercises 0]) 20 100 20)
   )
 
